8a6ceee37a1cceaf7911628b22a0f0b8c79bfa5e
Andrew Shaffer <darawk@gmail.com>
2016-05-22 14:56:28 -0700

    more comments

--- mnist.py
+++ mnist.py
@@ -18,6 +18,9 @@ b = tf.Variable(tf.zeros([10]))              # Bias [10]. Prior probability of e
 #
 
 y = tf.nn.softmax(tf.matmul(x, W) + b) # xW + b is how we classify an image or set of images
+                                       # softmax is a logistic function (sigmoid curve)
+                                       # normalized to be a probability distribution
+                                       # (values between [0,1] that sum to 1)
 
 #
 # Loss function (cross-entropy)
@@ -27,7 +30,7 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 #
-# Training
+# Training - Gradient descent back-propagation
 #
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
@@ -42,7 +45,7 @@ init = tf.initialize_all_variables()
 # Read in our training data
 #
 
-mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)  # Grab the images with the classifications specified as 'one_hot' vectors.
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)  # Grab the images with the classifications specified as 'one hot' vectors.
                                                                 # That is, [0, 1, 0, 0, 0, 0, 0,  0, 0, 0] would be an output vector for the number 1.
 
 #
@@ -55,7 +58,6 @@ sess.run(init)
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)              # Retrieve 100 samples from our data set of images (xs) and ys (classifications)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})    # Swap our random samples into the x and y placeholders, respectively
-
 
 #
 # Evaluation
